6efba1f9b4e620b41df6f6f9c808082624928a82
William Arustamyan <williamarustamyan@workfront.com>
2020-05-02 17:18:36 +0400

    Added simple variable declaration

--- src/com/jfk/Application.java
+++ src/com/jfk/Application.java
@@ -5,4 +5,15 @@ package com.jfk;
  */
 
 public class Application {
+
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 45;
+        int z = y + x;
+        int k = x + y;
+        
+
+
+        System.out.println("taest");
+    }
 }
