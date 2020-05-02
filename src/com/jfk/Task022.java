2b92aad716dd58181377cf3ded09080c0704e7d9
Artur Chilingaryan <arturchil@gmail.com>
2020-05-03 00:54:46 +0400

    Task 021, 022

---
+++ src/com/jfk/Task022.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class Task022 {
+    public static void main(String[] args) {
+        int a = 33;
+        int b = 99;
+        int c = 100;
+        int min = (a < b) ? a : b;
+        min = (min < c) ? min : c;
+        System.out.println("min = " + min);
+        System.out.println("main = " + Math.min(Math.min(a, b), c));
+    }
+}
