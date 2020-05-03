e2ac6581c5d9acb723d72f2a0942375dc3a7f4fd
Artur Chilingaryan <arturchil@gmail.com>
2020-05-04 01:24:30 +0400

    Task 038-040

---
+++ src/com/jfk/Lesson02/Task038.java
@@ -0,0 +1,15 @@
+package com.jfk.Lesson02;
+
+import java.util.Arrays;
+
+public class Task038 {
+    public static void main(String[] args) {
+        int a = 2;//7;
+        int b = 4;//99;
+        int c = 8;//134;
+        int d = 16;//100;
+        int[] array = {a, b, c, d};
+        Arrays.sort(array);
+        System.out.println(((array[1] * array[1]) == (array[2] * array[0])) && ((array[2] * array[2]) == (array[3] * array[1])));
+    }
+}
