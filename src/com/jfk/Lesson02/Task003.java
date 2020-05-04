0b95cbac01eef1f5ea19c3c7cf40bf35219a7992
Artur Chilingaryan <arturchil@gmail.com>
2020-05-04 20:08:33 +0400

    Task 001-005

---
+++ src/com/jfk/Lesson02/Task003.java
@@ -0,0 +1,18 @@
+package com.jfk.Lesson02;
+
+public class Task003 {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 17;
+        System.out.println(expression(x, y));
+    }
+    public static double expression(double x, double y){
+        double sum = 0;
+        sum = Math.atan(Math.abs(x*x - y) / (x*x +y*y));
+        sum = sum + Math.log10(x*x + 1);
+
+        //System.out.println(Math.log10(100));
+        //System.out.println(Math.log(8) / Math.log(2));
+        return sum;
+    }
+}
