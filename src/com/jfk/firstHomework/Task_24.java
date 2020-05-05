8551fb4ebe54001ca839560e9cbe50e931536780
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 22:51:28 +0400

    Task 24: Find two equal numbers which have value 2.

---
+++ src/com/jfk/firstHomework/Task_24.java
@@ -0,0 +1,26 @@
+package com.jfk.firstHomework;
+
+public class Task_24 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        int c = 2;
+
+        int eqaulNumber = 0;
+
+        if (a == b) {
+            eqaulNumber = a;
+        } else if (b == c) {
+            eqaulNumber = b;
+        } else if (a == c) {
+            eqaulNumber = a;
+        }
+
+        if (eqaulNumber == 2){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
