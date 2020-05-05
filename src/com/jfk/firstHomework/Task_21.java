25309b7a2cb8d0ff20b8dd5428c2ee9320b54b73
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 22:15:39 +0400

    Task 21: Find the greatest number.

---
+++ src/com/jfk/firstHomework/Task_21.java
@@ -0,0 +1,28 @@
+package com.jfk.firstHomework;
+
+public class Task_21 {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 30;
+        int c = 20;
+
+        int max = 50;
+
+        if (a > b) {
+            if (a > c) {
+                max = a;
+            } else {
+                max = c;
+            }
+        } else if (b > c) {
+            max = b;
+        } else {
+            max = c;
+        }
+        System.out.println("The max number is: " + max);
+    }
+}
+
+
