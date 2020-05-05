bf5cdc0fcf9ece6bb8924161990e06154d846cb4
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 23:42:52 +0400

    Task 25: Check if the numbers can make a triangle.

---
+++ src/com/jfk/firstHomework/Task_25.java
@@ -0,0 +1,23 @@
+package com.jfk.firstHomework;
+
+public class Task_25 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 7;
+
+        if (a + b > c) {
+            if (a + c > b) {
+                if (b + c > a) {
+                    System.out.println("y=1");
+                } else {
+                    System.out.println("y=2");
+                }
+            } else {
+                System.out.println("y=2");
+            }
+        } else {
+            System.out.println("y=2");
+        }
+    }
+}
