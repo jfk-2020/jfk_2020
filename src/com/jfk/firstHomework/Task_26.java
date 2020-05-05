fe317f07e43ec81dd78ef48b33090a11d7efa0bb
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 23:50:21 +0400

    Task 26: Check if at least on number is even.

---
+++ src/com/jfk/firstHomework/Task_26.java
@@ -0,0 +1,16 @@
+package com.jfk.firstHomework;
+
+public class Task_26 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+            System.out.println("1");
+
+        } else {
+            System.out.println("2");
+        }
+    }
+}
