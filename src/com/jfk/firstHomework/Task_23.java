8542667e01f081a468b118ce1777279ef1807ec7
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 22:51:02 +0400

    Task 23: Find two equal numbers.

---
+++ src/com/jfk/firstHomework/Task_23.java
@@ -0,0 +1,19 @@
+package com.jfk.firstHomework;
+
+public class Task_23 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        int c = 10;
+
+        if (a == b) {
+            System.out.println(true);
+        } else if (b == c) {
+            System.out.println(true);
+        } else if (a == c) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
