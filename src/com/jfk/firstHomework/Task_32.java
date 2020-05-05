b8ec3956b5775ab8aa705223bbcd08235c47a0ce
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 01:37:24 +0400

    Task 32: Print the smallest number of 4 integers.

---
+++ src/com/jfk/firstHomework/Task_32.java
@@ -0,0 +1,44 @@
+package com.jfk.firstHomework;
+
+public class Task_32 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int c = 5;
+        int d = 3;
+
+        int min = 0;
+
+        if (a < b) {
+            if (a < c) {
+                if (a < d) {
+                    min = a;
+                } else {
+                    min = d;
+                }
+            } else {
+                min = c;
+                if (d < c) {
+                    min = d;
+                } else {
+                    min = c;
+                }
+            }
+        } else {
+            if (b < c) {
+                if (b < d) {
+                    min = b;
+                } else {
+                    min = d;
+                }
+            } else {
+                if (c < d) {
+                    min = c;
+                } else {
+                    min = d;
+                }
+            }
+        }
+        System.out.println("The min number is: " + min);
+    }
+}
