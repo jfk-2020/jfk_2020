6dd517e4303f82e2ad7d62458e2f6d3ea97da002
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 00:03:53 +0400

    Task 27: Check Arithmetic progression numbers.

---
+++ src/com/jfk/firstHomework/Task_27.java
@@ -0,0 +1,15 @@
+package com.jfk.firstHomework;
+
+public class Task_27 {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 4;
+        int c = 6;
+
+        if (c - b == b - a){
+            System.out.println(true);
+        } else{
+            System.out.println(false);
+        }
+    }
+}
