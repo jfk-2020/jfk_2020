170c583e3d924cebcbcd5236351dde56cf10f1e7
tatevmel <tatevmelikyan90@gmail.com>
2020-05-05 22:50:07 +0400

    Task 22: Find the smallest number.

---
+++ src/com/jfk/firstHomework/Task_22.java
@@ -0,0 +1,26 @@
+package com.jfk.firstHomework;
+
+public class Task_22 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int c = 20;
+
+        int min = 5;
+
+        if (a < b) {
+            if (a < c) {
+                min = a;
+            } else {
+                min = c;
+            }
+        } else if (b < c) {
+            min = b;
+        } else {
+            min = c;
+        }
+        System.out.println("The max number is: " + min);
+    }
+}
+
