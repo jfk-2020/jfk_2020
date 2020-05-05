e0737db5a88ee25d74a310e72eae0aeaffba6f30
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 00:10:00 +0400

    Task 28: Check Geometric progression numbers.

---
+++ src/com/jfk/firstHomework/Task_28.java
@@ -0,0 +1,15 @@
+package com.jfk.firstHomework;
+
+public class Task_28 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 8;
+
+        if (c / b == b / a) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
