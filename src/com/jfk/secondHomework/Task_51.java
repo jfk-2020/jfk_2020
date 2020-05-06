8848bb9285b20119d909ae26ce766c9c9f2786f6
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 23:10:53 +0400

    Task 51: Three digit number.

---
+++ src/com/jfk/secondHomework/Task_51.java
@@ -0,0 +1,13 @@
+package com.jfk.secondHomework;
+//Check if number of points equals the sum of number of decimals and number of hundreds
+public class Task_51 {
+    public static void main (String[] args){
+        int number = 379;
+
+        int f = number / 100;
+        int m = (number % 100)/10;
+        int l = number % 10;
+
+        System.out.println(l == m + f);
+    }
+}
