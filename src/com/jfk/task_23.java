d7f5f93e521e95003c33a6873c140ba6cc590f7f
ArmenHanisyan <hanisyan94@gmail.com>
2020-05-06 06:09:15 -0700

    task 23

---
+++ src/com/jfk/task_23.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+public class task_23 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 9;
+        int c = 9;
+
+        if (a == b) {
+            System.out.println(true);
+        } else if (b == c) {
+            System.out.println(true);
+        } else if (a == c) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
