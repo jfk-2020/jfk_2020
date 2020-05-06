6954d01e7ad09b23f924845ea20e470074a25ffb
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 10:58:59 +0400

    Task 34: Check if sum of two numbers equals to the sum of the other two numbers.

---
+++ src/com/jfk/firstHomework/Task_34.java
@@ -0,0 +1,20 @@
+package com.jfk.firstHomework;
+
+public class Task_34 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 7;
+        int d = 8;
+
+        if (a + b == c + d) {
+            System.out.println(true);
+        } else if (a + c == b + d) {
+            System.out.println(true);
+        } else if (a + d == b + c) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
