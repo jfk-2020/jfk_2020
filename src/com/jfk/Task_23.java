3d6c24fe2b7e5f652a71d24a36cd1411e0136863
Vahan <Vahanaleq1988@gmail.com>
2020-05-06 14:08:54 +0400

    Task_23-24

---
+++ src/com/jfk/Task_23.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_23 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 40;
+        int c = 2;
+        boolean jhisht = (a == 1 || b == 1 || c == 1);
+        System.out.println(jhisht);
+    }
+}
---
+++ src/com/jfk/Task_24.java
@@ -0,0 +1,11 @@
+package com.jfk;
+
+public class Task_24 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = 2;
+        boolean y = (a == 2 && b == 2 || a ==2 && c == 2 || b == 2 && c == 2);
+        System.out.println(y);
+    }
+}
