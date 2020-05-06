1a7a0188961c134067257b86896046157e8c7d47
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 23:20:32 +0400

    Task 52: Three digit number: check if there are same digits.

---
+++ src/com/jfk/secondHomework/Task_52.java
@@ -0,0 +1,18 @@
+package com.jfk.secondHomework;
+
+//Check if there are the same numbers in the three digit number
+public class Task_52 {
+    public static void main(String[] args) {
+        int number = 373;
+
+        int f = number / 100;
+        int m = (number % 100) / 10;
+        int l = number % 10;
+
+        if (f == m || f == l || m == l) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
