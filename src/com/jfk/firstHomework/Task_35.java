eb14aa11802d2475b31048fe88f83efc50edc570
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 11:12:08 +0400

    Task 35: Check if one of the numbers equals to the sum of other three numbers.

---
+++ src/com/jfk/firstHomework/Task_35.java
@@ -0,0 +1,22 @@
+package com.jfk.firstHomework;
+
+public class Task_35 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = 3;
+        int d = 10;
+
+        if (a == b+c+d) {
+            System.out.println(true);
+        }else if(b == a+c+d){
+            System.out.println(true);
+        }else if(c == a+b+d){
+            System.out.println(true);
+        }else if(d == a+b+c){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
