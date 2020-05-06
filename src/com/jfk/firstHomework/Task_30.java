90af2728846ff808ad8ccbce7f6e8984b80bfa7e
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 13:06:25 +0400

    Task 30: Small change.

--- src/com/jfk/firstHomework/Task_30.java
+++ src/com/jfk/firstHomework/Task_30.java
@@ -6,18 +6,18 @@ public class Task_30 {
         int b = 1;
         int c = 5;
 
-        if (a > b && a > c && b > c) {
+        if (a > b && b > c) {
             System.out.println(a + " " + b + " " + c);
-        } else if (b > a && b > c && c > a) {
-            System.out.println(b + " " + c + " " + a);
-        } else if (c > a && c > b && b > a) {
-            System.out.println(c + " " + b + " " + a);
-        } else if (b > a && b > c && a > c) {
-            System.out.println(b + " " + a + " " + c);
-        } else if (c > b && a > b && c > a) {
-            System.out.println(c + " " + a + " " + b);
-        } else if (c > b && a > b && a > c) {
+        } else if (a > c && c > b) {
             System.out.println(a + " " + c + " " + b);
+        } else if (b > a && a > c) {
+            System.out.println(b + " " + a + " " + c);
+        } else if (b > c && c > a) {
+            System.out.println(b + " " + c + " " + a);
+        } else if (c > a && a > b) {
+            System.out.println(c + " " + a + " " + b);
+        } else if (c > b && b > a) {
+            System.out.println(c + " " + b + " " + a);
         }
     }
 }
