68922b4e3ba171ddf67bb81c99c02ec327963b42
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 12:58:03 +0400

    Task 38: Check if the numbers are in Geometric progression.

---
+++ src/com/jfk/firstHomework/Task_38.java
@@ -0,0 +1,16 @@
+package com.jfk.firstHomework;
+
+public class Task_38 {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double c = 4;
+        double d = 8;
+
+        if (d / c == c / b && c / b == b / a) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
