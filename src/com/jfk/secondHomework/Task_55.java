3fb3ab42f32624ead184b721b466850aeb2295ac
tatevmel <tatevmelikyan90@gmail.com>
2020-05-07 00:20:00 +0400

    Task 55: Print the smallest number.

---
+++ src/com/jfk/secondHomework/Task_55.java
@@ -0,0 +1,27 @@
+package com.jfk.secondHomework;
+
+//find the smallest digit of a three digit number
+public class Task_55 {
+    public static void main(String[] args) {
+        int number = 532;
+
+        int f = number / 100;
+        int m = (number % 100) / 10;
+        int l = number % 10;
+
+        if (f < m) {
+            if (f < l) {
+                System.out.println(f);
+            } else {
+                System.out.println(l);
+            }
+        } else {
+            if (m < l) {
+                System.out.println(m);
+            } else {
+                System.out.println(l);
+            }
+        }
+    }
+}
+
