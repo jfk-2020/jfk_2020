3a28c1bd95700e872d70e3f1af4964cf2127bd74
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 10:20:22 +0400

    Task 33: Check if at least one of the numbers is 1.

---
+++ src/com/jfk/firstHomework/Task_33.java
@@ -0,0 +1,16 @@
+package com.jfk.firstHomework;
+
+public class Task_33 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = 3;
+        int d = 13;
+
+        if (a == 1 || b == 1 || c == 1 || d == 1) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
