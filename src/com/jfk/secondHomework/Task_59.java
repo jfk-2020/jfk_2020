474ec78e1cef9e99ba3a1e18498a68254b943315
tatevmel <tatevmelikyan90@gmail.com>
2020-05-07 01:14:04 +0400

    Task 59: Print the numbers by ascending order.

---
+++ src/com/jfk/secondHomework/Task_59.java
@@ -0,0 +1,27 @@
+package com.jfk.secondHomework;
+
+//print the numbers by ascending order
+
+public class Task_59 {
+    public static void main(String[]args){
+    int number = 321;
+
+    int f = number / 100;
+    int m = (number % 100) / 10;
+    int l = number % 10;
+
+    if(f < m && m < l){
+        System.out.println(f + " " + m + " " + l);
+    }else if(f < l && l < m){
+        System.out.println(f + " " + l + " " + m);
+    }else if(m < l && l < f){
+        System.out.println(m + " " + l + " " + f);
+    }else if(m < f && f < l){
+        System.out.println(m + " " + f + " " + l);
+    }else if(l < m && m < f){
+        System.out.println(l + " " + m + " " + f);
+    }else if(l < f && f < m){
+        System.out.println(l + " " + f + " " + m);
+    }
+        }
+}
