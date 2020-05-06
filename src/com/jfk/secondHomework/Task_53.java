24030ed612d92addd8dc8c07b81abeaba248b1f1
tatevmel <tatevmelikyan90@gmail.com>
2020-05-06 23:50:49 +0400

    Task 53: Task 53.

---
+++ src/com/jfk/secondHomework/Task_53.java
@@ -0,0 +1,19 @@
+package com.jfk.secondHomework;
+
+public class Task_53 {
+    public static void main(String[] args) {
+        int k = 400;
+        int myNumber = 335;
+
+        int f = myNumber / 100;
+        int m = (myNumber % 100) / 10;
+        int l = myNumber % 10;
+        int sum = f + m + l;
+
+        if (myNumber > k) {
+            System.out.println(myNumber - sum);
+        } else {
+            System.out.println(l - myNumber);
+        }
+    }
+}
