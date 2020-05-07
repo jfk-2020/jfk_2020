9ba7f5d2c2852a247c82228a26cabb99c635ab2a
sergeypetrosyan <sergopetrosyan@gmail.com>
2020-05-07 12:28:02 +0400

    Task_51

---
+++ src/Tasks_from_51_to_70/Task_051.java
@@ -0,0 +1,14 @@
+package Tasks_from_51_to_70;
+
+public class Task_051 {
+    public static void main(String[] args) {
+
+        int number = 156;
+        int f = (number % 10);
+        int m = (number % 100) / 10;
+        int l = (number % 1000) / 100;
+
+        boolean t = (f == m + l);
+        System.out.println(t);
+    }
+}
