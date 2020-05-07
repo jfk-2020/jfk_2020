0dcc25f986f5f3d993b2dcb0965fa3cd97a48968
Vahan <Vahanaleq1988@gmail.com>
2020-05-07 13:31:31 +0400

    Task_23-24

---
+++ src/com/jfk/Task26.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+public class Task26 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 15;
+        int c = 37;
+
+        if (a % 2 ==0 || b % 2 ==0 || c % 2 ==0){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("2");
+        }
+    }
+}
+    }
+}
+
