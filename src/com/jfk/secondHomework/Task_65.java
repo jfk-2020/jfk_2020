0679e8fa758690ea6536afd6d0476f13e6402dfc
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:23:10 +0400

    Task 65: Print y = 12 or y = 0 depending on the four digit number.

--- src/com/jfk/secondHomework/Task_65.java
+++ src/com/jfk/secondHomework/Task_65.java
@@ -4,17 +4,14 @@ public class Task_65 {
     public static void main(String[] args) {
         int number = 5626;
 
-        int first = number / 1000;
-        int mid = (number % 1000) / 100;
         int mid2 = (number % 100) / 10;
         int last = number % 10;
         int y;
-        int product = last * mid2;
-        if (product == 12) {
+        if (last * mid2 == 12) {
             y = 12;
         } else {
             y = 0;
         }
-        System.out.println(y);
+        System.out.println("y = " + y);
     }
 }
