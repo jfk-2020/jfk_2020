7eee8877a0a51c83ab73a85b78ef7aeadcbca72a
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:24:11 +0400

    Task 68: Print the product result depending on the four digit number.

--- src/com/jfk/secondHomework/Task_68.java
+++ src/com/jfk/secondHomework/Task_68.java
@@ -4,15 +4,11 @@ public class Task_68 {
     public static void main(String[] args) {
         int number = 1231;
 
-        int first = number / 1000;
         int mid = (number % 1000) / 100;
-        int mid2 = (number % 100) / 10;
         int last = number % 10;
 
-        int product = last * mid;
-
         if (last > mid) {
-            System.out.println(product);
+            System.out.println(last * mid);
         } else {
             System.out.println(1);
         }
