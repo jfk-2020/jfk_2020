11efe98a8df11dfdf29aa408fb1ac4188c62a40f
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:21:53 +0400

    Task 62: Print subtraction, if the four digit number is less than 5000.

--- src/com/jfk/secondHomework/Task_62.java
+++ src/com/jfk/secondHomework/Task_62.java
@@ -2,19 +2,17 @@ package com.jfk.secondHomework;
 
 public class Task_62 {
     public static void main(String[] args) {
-        int number = 4578;
+        int number = 5571;
 
-        int first = number / 1000;
-        int mid = (number % 1000) / 100;
-        int mid2 = (number % 100) / 10;
-        int last = number % 10;
-
-        int a = number / (last + mid);
-        int b = number / (first + mid2);
-
-        if(number < 5000){
+        if (number < 5000) {
+            int mid = (number % 1000) / 100;
+            int last = number % 10;
+            int a = number / (last + mid);
             System.out.println(a);
-        }else{
+        } else {
+            int first = number / 1000;
+            int mid2 = (number % 100) / 10;
+            int b = number / (first + mid2);
             System.out.println(b);
         }
     }
