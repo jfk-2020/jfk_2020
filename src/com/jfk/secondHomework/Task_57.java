cfb0e7dce72db298cb0cd83274c232aec1604b1f
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:41:05 +0400

    Task 57: Check if the three digit number is bigger than 300.

--- src/com/jfk/secondHomework/Task_57.java
+++ src/com/jfk/secondHomework/Task_57.java
@@ -2,19 +2,16 @@ package com.jfk.secondHomework;
 
 public class Task_57 {
     public static void main(String[] args) {
-        int number = 231;
+        int number = 137;
 
-        int f = number / 100;
-        int m = (number % 100) / 10;
-        int l = number % 10;
+        double f = number / 100;
+        double m = (number % 100) / 10;
+        double l = number % 10;
 
-        int subtraction1 = m - l;
-        int subtraction2 = f - l;
-        int givenNumber = 300;
-        if (number > givenNumber) {
-            System.out.println(subtraction1);
+        if (number > 300) {
+            System.out.println(m / l);
         } else {
-            System.out.println(subtraction2);
+            System.out.println(f / l);
         }
     }
 }
