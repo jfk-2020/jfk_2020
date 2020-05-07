0d23f791d0293e088eb25527e1ccefe89b4832f5
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-07 14:53:02 +0400

    Task54

---
+++ src/Homework/Task_54.java
@@ -0,0 +1,31 @@
+package Homework;
+
+public class Task_54 {
+    public static void main(String[] args) {
+
+        int i = 958;
+        int miavor = i % 10;
+        int tasnavor = i % 100 / 10;
+        int haryuravor = i / 100;
+
+        System.out.println(haryuravor + " Haryuravor");
+        System.out.println(tasnavor + " Tasnavor");
+        System.out.println(miavor + " Miavor");
+
+        if (miavor >= tasnavor) {
+            if (miavor >= haryuravor
+            ) {
+                System.out.println(miavor + " max");
+            } else {
+                System.out.println(haryuravor + " max");
+            }
+        }
+        else {if(tasnavor>=haryuravor){
+            System.out.println(tasnavor + " max");
+        }else{
+            System.out.println( haryuravor + "max");
+        }
+
+        }
+    }
+}
