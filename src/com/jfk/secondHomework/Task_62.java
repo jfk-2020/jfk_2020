c4fc012d9f5dc0aeb86dfbb79ddc60d75049dd21
tatevmel <tatevmelikyan90@gmail.com>
2020-05-07 15:35:09 +0400

    Task 62: Check if the four digit number is less than 5000.

---
+++ src/com/jfk/secondHomework/Task_62.java
@@ -0,0 +1,21 @@
+package com.jfk.secondHomework;
+
+public class Task_62 {
+    public static void main(String[] args) {
+        int number = 4578;
+
+        int first = number / 1000;
+        int mid = (number % 1000) / 100;
+        int mid2 = (number % 100) / 10;
+        int last = number % 10;
+
+        int a = number / (last + mid);
+        int b = number / (first + mid2);
+
+        if(number < 5000){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
