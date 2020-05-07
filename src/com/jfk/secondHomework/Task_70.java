a7e5b17e6c84cbcfbf3bf22ad7b60ebf085bb973
tatevmel <tatevmelikyan90@gmail.com>
2020-05-07 16:22:22 +0400

    Task 70: Print either 0 or 1 depending on the four digit number.

---
+++ src/com/jfk/secondHomework/Task_70.java
@@ -0,0 +1,22 @@
+package com.jfk.secondHomework;
+
+public class Task_70 {
+    public static void main(String[] args) {
+
+        int number = 1234;
+
+        int first = number / 1000;
+        int mid = (number % 1000) / 100;
+        int mid2 = (number % 100) / 10;
+        int last = number % 10;
+
+        int y;
+        int product = last * mid2 * mid * first;
+        if (product > 200) {
+            y = 0;
+        } else {
+            y = 1;
+        }
+        System.out.println(y);
+    }
+}
