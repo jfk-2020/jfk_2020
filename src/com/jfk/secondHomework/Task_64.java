cfed97d85e8a62cba0e2460e8acdefa763882024
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:22:31 +0400

    Task 64: Print s or d depending on the four digit number.

--- src/com/jfk/secondHomework/Task_64.java
+++ src/com/jfk/secondHomework/Task_64.java
@@ -4,8 +4,6 @@ public class Task_64 {
     public static void main(String[] args) {
         int number = 5623;
 
-        int first = number / 1000;
-        int mid = (number % 1000) / 100;
         int mid2 = (number % 100) / 10;
         int last = number % 10;
 
