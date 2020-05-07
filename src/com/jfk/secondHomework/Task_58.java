3532ad57fe0fd96bc0a8b7c06f563946b49cd8f2
tatevmel <tatevmelikyan90@gmail.com>
2020-05-08 00:40:29 +0400

    Task 58: Print a or b if the result of the sum is less then 5.

--- src/com/jfk/secondHomework/Task_58.java
+++ src/com/jfk/secondHomework/Task_58.java
@@ -4,15 +4,13 @@ public class Task_58 {
     public static void main(String[] args) {
         int number = 281;
 
-        int f = number / 100;
         int m = (number % 100) / 10;
         int l = number % 10;
 
-        int sum = m + l;
         char c;
-        if(sum < 5){
+        if (m + l < 5) {
             c = 'a';
-        }else{
+        } else {
             c = 'b';
         }
         System.out.println(c);
