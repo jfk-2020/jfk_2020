b2f87cebbe0605224755a0ef36db29803ad6cd31
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-08 11:43:14 +0400

    Task64

---
+++ src/Homework/Task_64.java
@@ -0,0 +1,23 @@
+package Homework;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Task_64 {
+    public static void main(String[] args) {
+        int i = 1314;
+        int miavor = i % 10;
+        int tasnavor = i % 100 /10 ;
+        int haryuravor = i %1000 / 100;
+        int hazaravor = i /1000;
+
+
+
+         if (tasnavor + miavor ==5){ char y = 's' ;
+             System.out.println(y);}
+         else { char y = 'd';System.out.println(y);}
+
+
+    }
+
+
+}
