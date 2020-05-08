62684c0181a30c086ecae5d5698948564c94db70
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-08 12:40:18 +0400

    Task68

--- src/Homework/Task_68.java
+++ src/Homework/Task_68.java
@@ -3,7 +3,7 @@ package Homework;
 public class Task_68 {
     public static void main(String[] args) {
 
-        int i = 1321;
+        int i = 1322;
         int miavor = i % 10;
         int tasnavor = i % 100 /10 ;
         int haryuravor = i %1000 / 100;
