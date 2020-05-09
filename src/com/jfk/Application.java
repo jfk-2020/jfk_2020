3c26cebd6e0b8963f6d7e1bdb7cf57f19efac7ce
William Arustamyan <williamarustamyan@workfront.com>
2020-05-09 12:47:29 +0400

    switch statement

--- src/com/jfk/Application.java
+++ src/com/jfk/Application.java
@@ -7,13 +7,35 @@ package com.jfk;
 public class Application {
 
     public static void main(String[] args) {
-        int x = 30;
+        int x = 5000;
         int y = 45;
         int z = y + x;
         int k = x + y;
-        
+        if (!(!((x > 20 && x < 47) && y < 45) || x != 45 || x == 30 ||
+                !((x > 20 && x < 47) && y < 45) || x != 45 || x == 30) && !((x > 20 && x < 47) && y < 45) || x != 45 || x == 30) {
+
+        } else {
+        }
 
 
-        System.out.println("taest");
+        switch (x) {
+            case 5000:
+                System.out.println("5000");
+                break;
+            case 10000:
+                System.out.println("10000");
+                break;
+            case 20000:
+                System.out.println("20000");
+                break;
+            case 30000:
+                System.out.println("30000");
+                break;
+            case -1:
+                System.out.println("AYL GUMAR");
+                break;
+            default:
+                System.out.println("LOCK");
+        }
     }
 }
