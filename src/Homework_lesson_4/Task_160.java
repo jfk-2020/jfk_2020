80181b215e31e1197348c0d25f9e47cc59f644e1
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-11 00:06:43 +0400

    Task160

--- src/Homework_lesson_4/Task_160.java
+++ src/Homework_lesson_4/Task_160.java
@@ -4,8 +4,8 @@ public class Task_160 {
     public static void main(String[] args) {
         int x = 16;
         int y =1;
-        for (int i = 100;i>=100&& i< 1000; i++)
-        { if (  i*i %x == 0 ){y = i*i / x;;break;}}
+        for (int i = 1;i>=1&& i< 1000; i++)
+        { if (  i*i %x == 0 ){y = i*i / x; if (y >99 && y < 1000){break;}}}
         System.out.println(y);
     }
 }
