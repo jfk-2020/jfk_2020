a29975b05d0bedac06acba09a17825c95cab354f
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 15:45:53 +0400

    Task 153: print the product of all numbers that n % i == 3.

---
+++ src/com/jfk/thirdHomework/Task_154.java
@@ -0,0 +1,18 @@
+package com.jfk.thirdHomework;
+
+public class Task_154 {
+    public static void main(String[] args) {
+
+//print the product of all numbers that n % i == 3
+        int n = 120;
+        int product = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 3) {
+                product = product * i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(product);
+    }
+}
