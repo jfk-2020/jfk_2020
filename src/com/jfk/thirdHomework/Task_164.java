6cd6630b554c44ef7ec9bb134c8c57d3a8b3eeb8
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 01:13:07 +0400

    Task 164: Print the smallest three digit number.

---
+++ src/com/jfk/thirdHomework/Task_164.java
@@ -0,0 +1,16 @@
+package com.jfk.thirdHomework;
+
+public class Task_164 {
+    public static void main(String[] args) {
+
+        int n = 13;
+        for (int i = 100; i <= 999; i++) {
+
+            double sr = Math.sqrt(i);
+            if (sr > n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
