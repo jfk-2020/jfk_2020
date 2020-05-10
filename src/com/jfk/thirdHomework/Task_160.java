77849c07533b077d77188813b275dc4799b90dba
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 18:29:45 +0400

    Task 160: Print the smallest three digit number.

---
+++ src/com/jfk/thirdHomework/Task_160.java
@@ -0,0 +1,16 @@
+package com.jfk.thirdHomework;
+
+public class Task_160 {
+    public static void main(String[] args) {
+        //Print the smallest three digit number that after * 16 is a perfect square of a natural number
+
+        for (int i = 100; i <= 999; i++) {
+            double sr = Math.sqrt(i * 16);
+
+            if (((sr - Math.floor(sr)) == 0)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
