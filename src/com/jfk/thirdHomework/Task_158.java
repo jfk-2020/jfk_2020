26ff29b610c1d3457147ac1f3bf1a363f5962802
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 17:15:56 +0400

    Task 158: print the sum of all the three digit numbers.

--- src/com/jfk/thirdHomework/Task_158.java
+++ src/com/jfk/thirdHomework/Task_158.java
@@ -3,11 +3,11 @@ package com.jfk.thirdHomework;
 public class Task_158 {
     public static void main(String[] args) {
         //print the sum of all the three digit numbers that % 2 !=0 and % 3 !=0
-        int product = 1;
+        long product = 1;
 
         for (int i = 100; i <= 999; i++) {
             if (i % 2 != 0 && i % 3 != 0) {
-               product = product + i;
+               product = product * i;
                 System.out.println(i);
             }
         }
