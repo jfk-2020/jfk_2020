cdf5527446fcf88c0bc00ecc88a72a83a128ecf0
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-10 22:36:03 +0400

    Task159

---
+++ src/Homework_lesson_4/Task_159.java
@@ -0,0 +1,14 @@
+package Homework_lesson_4;
+
+public class Task_159 {
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 4;
+        int sum = 1;
+        // eranish tvov anhamemat mec klini
+        for (int i = 10 ;i >= 10 && i < 100 ; i++){
+            if (i % 3 == 1 && i % 4 == 2){sum = sum * i ;}
+        }
+        System.out.println(sum);
+    }
+}
