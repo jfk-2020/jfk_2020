3ad923210783b6cd017c72276ae312d621e80c4f
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 15:24:28 +0400

    Task 151: Print the sum of all natural numbers.

---
+++ src/com/jfk/thirdHomework/Task_151.java
@@ -0,0 +1,17 @@
+package com.jfk.thirdHomework;
+
+public class Task_151 {
+    public static void main(String[] args) {
+
+        int n = 64;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
