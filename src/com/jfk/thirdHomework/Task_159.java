27d401c3c9c8bef4a3b84a1044216b8a22a98610
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 17:36:19 +0400

    Task 159: print the product of all the three digit numbers.

---
+++ src/com/jfk/thirdHomework/Task_159.java
@@ -0,0 +1,18 @@
+package com.jfk.thirdHomework;
+
+public class Task_159 {
+    public static void main(String[] args) {
+
+        //print the product of all the three digit numbers that % 3 ==1 and % 4 ==2
+        long product = 1;
+
+        for (int i = 100; i <= 555; i++) {
+            if (i % 3 == 1 && i % 4 == 2) {
+                product = product * i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(product);
+    }
+
+}
