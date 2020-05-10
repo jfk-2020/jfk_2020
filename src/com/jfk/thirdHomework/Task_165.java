24aa2bb3ba5bd4ae58b1e840f9d399859031fea9
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 01:50:54 +0400

    Task 165: Print true or false according to the given n value.

---
+++ src/com/jfk/thirdHomework/Task_165.java
@@ -0,0 +1,18 @@
+package com.jfk.thirdHomework;
+
+public class Task_165 {
+    public static void main(String[] args) {
+        int n = 81;
+        boolean t = true;
+
+        while (n > 1) {
+            if (n % 3 == 0) {
+                n = n / 3;
+            } else {
+                t = false;
+                break;
+            }
+        }
+        System.out.println(t);
+    }
+}
