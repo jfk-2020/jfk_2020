3d3cc0fa972c67ac1c42e7b08a4ff7ef2bf0259b
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 16:58:25 +0400

    Task 156: print the sum of all the two digit numbers.

---
+++ src/com/jfk/thirdHomework/Task_156.java
@@ -0,0 +1,16 @@
+package com.jfk.thirdHomework;
+
+public class Task_156 {
+    public static void main(String[] args) {
+
+        //print the sum of all the two digit numbers that % 3 == 0 and % 5 == 0
+        int sum = 0;
+
+        for (int i = 12; i <= 99; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+    }
+}
