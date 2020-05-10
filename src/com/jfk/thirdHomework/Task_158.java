bea193fd4ce61243a791c6e380d6c68bc61dc7a6
tatevmel <tatevmelikyan90@gmail.com>
2020-05-10 17:36:13 +0400

    Task 158: print the product of all the three digit numbers.

--- src/com/jfk/thirdHomework/Task_158.java
+++ src/com/jfk/thirdHomework/Task_158.java
@@ -2,7 +2,7 @@ package com.jfk.thirdHomework;
 
 public class Task_158 {
     public static void main(String[] args) {
-        //print the sum of all the three digit numbers that % 2 !=0 and % 3 !=0
+        //print the product of all the three digit numbers that % 2 !=0 and % 3 !=0
         long product = 1;
 
         for (int i = 100; i <= 999; i++) {
