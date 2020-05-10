61af5a8d9e09ba23a20fc2e5278ae85a7c5df0e7
Artur Chilingaryan <arturchil@gmail.com>
2020-05-10 12:59:36 +0400

    Task 076-080

---
+++ src/com/jfk/Lesson04/Task079.java
@@ -0,0 +1,13 @@
+package com.jfk.Lesson04;
+
+public class Task079 {
+    public static void main(String[] args) {
+        for (int x = -4; x <=5; x++){
+            double y = -9;
+            if (x > 1) {
+                y = Math.log(x) / Math.log(4);
+            }
+            System.out.println("x = " + x + "; y = " + y);
+        }
+    }
+}
---
+++ src/com/jfk/Lesson04/Task080.java
@@ -0,0 +1,16 @@
+package com.jfk.Lesson04;
+
+public class Task080 {
+    public static void main(String[] args) {
+        for (int x = -5; x <= 5; x += 2) {
+            double y;
+//            if (x > 5) {
+//                y = Math.pow(2, 5 - x);
+//            } else {
+//                y = 7 - x;
+//            }
+            y = 7 - x;
+            System.out.println("x = " + x + "; y = " + y);
+        }
+    }
+}
