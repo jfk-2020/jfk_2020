a04e275b0539c75e27adb03b24baf4e66e1addc9
Vahan <Vahanaleq1988@gmail.com>
2020-05-10 15:48:20 +0400

    TASK 26

---
+++ src/com/jfk/Task_27.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class Task_27 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 9;
+        int c = 25;
+        boolean x=(b - a == 8 && c - b == 16);
+        System.out.println("x");
+    }
+}
+
+
