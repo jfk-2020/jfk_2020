e97743ea6e7e98c6835fda09b581576737e8518a
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 23:52:19 +0400

    Task 202: Print the sum of digits of the given number.

---
+++ src/com/jfk/thirdHomework/Task_202.java
@@ -0,0 +1,14 @@
+package com.jfk.thirdHomework;
+
+public class Task_202 {
+    public static void main(String[] args) {
+        int n = 3578;
+        int sum = 0;
+
+        while(n != 0){
+           sum += n % 10;
+           n = n / 10;
+        }
+        System.out.println(sum);
+    }
+}
