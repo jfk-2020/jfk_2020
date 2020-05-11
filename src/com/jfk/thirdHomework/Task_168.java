80204db0bc4aefe863fb8dee8219acec6a949b23
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 20:54:59 +0400

    Task 168: Print true or false if n is prime number.

---
+++ src/com/jfk/thirdHomework/Task_168.java
@@ -0,0 +1,16 @@
+package com.jfk.thirdHomework;
+
+public class Task_168 {
+    public static void main(String[] args) {
+
+        int num = 54;
+        boolean p = true;
+        for (int i = 2; i < num / 2; i++) {
+            if (num % i == 0) {
+                p = false;
+                break;
+            }
+        }
+        System.out.println(p);
+    }
+}
