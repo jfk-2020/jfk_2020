3c480abc160c45cb4dac571566eb5ff01f2df932
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 13:00:05 +0400

    Task 166: Print 0 or 1 according to the given n value.

---
+++ src/com/jfk/thirdHomework/Task_166.java
@@ -0,0 +1,19 @@
+package com.jfk.thirdHomework;
+
+public class Task_166 {
+    public static void main(String[] args) {
+        int y = 1;
+        int n = 64;
+
+        while (n > 1) {
+            if (n % 4 == 0) {
+                n = n / 4;
+
+            } else {
+                y = 0;
+                break;
+            }
+        }
+        System.out.println(y);
+    }
+}
