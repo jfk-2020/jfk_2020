3156b955d9da01aa1e1df2657eccea8aa5c8d000
tatevmel <tatevmelikyan90@gmail.com>
2020-05-12 01:03:42 +0400

    Task 205: Print the digits of a number from left to right.

---
+++ src/com/jfk/thirdHomework/Task_205.java
@@ -0,0 +1,23 @@
+package com.jfk.thirdHomework;
+
+public class Task_205 {
+    public static void main(String[] args) {
+        int n = 5327;
+        int reverse = 0;
+
+        while (n != 0) {
+            int a = n % 10;
+            reverse = reverse + a;
+
+            n = n / 10;
+            if (n != 0) {
+                reverse = reverse * 10;
+            }
+        }
+        while (reverse != 0) {
+
+            System.out.print(reverse % 10);
+            reverse = reverse / 10;
+        }
+    }
+}
