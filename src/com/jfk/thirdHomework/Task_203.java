58e709b145cc12320c131681074ef5f0470c4b43
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 23:54:42 +0400

    Task 203: Print the product of digits of the given number.

---
+++ src/com/jfk/thirdHomework/Task_203.java
@@ -0,0 +1,14 @@
+package com.jfk.thirdHomework;
+
+public class Task_203 {
+    public static void main(String[] args) {
+        int n = 1235;
+        int product = 1;
+
+        while(n != 0){
+            product *= n % 10;
+            n = n/10;
+        }
+        System.out.println(product);
+    }
+}
