33b6dadc212f5a5224fcd33ae1113eb0dbcbd370
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 23:51:59 +0400

    Task 201: Print the count of the given number.

--- src/com/jfk/thirdHomework/Task_201.java
+++ src/com/jfk/thirdHomework/Task_201.java
@@ -2,8 +2,9 @@ package com.jfk.thirdHomework;
 
 public class Task_201 {
     public static void main(String[] args) {
-        int n = 23775658;
+        int n = 237;
         int count = 0;
+
         while (n != 0) {
             n = n / 10;
             count++;
