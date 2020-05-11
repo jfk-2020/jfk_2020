cceb74259db7ea5edda30401e1a09e9a7bbe7219
tatevmel <tatevmelikyan90@gmail.com>
2020-05-11 20:57:39 +0400

    Task 170: Print the smallest number that is > n that is square of 2.

---
+++ src/com/jfk/thirdHomework/Task_170.java
@@ -0,0 +1,24 @@
+package com.jfk.thirdHomework;
+
+public class Task_170 {
+    public static void main(String[] args) {
+        int n = 16;
+        boolean t = true;
+        while (t) {
+            n = n + 1;
+            int k = n;
+            while (k > 1) {
+                if (k % 2 == 0) {
+                    k = k / 2;
+                } else {
+                    break;
+                }
+                if (k == 1) {
+                    t = false;
+                }
+            }
+        }
+        System.out.println(n);
+    }
+}
+
