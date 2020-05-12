71e26ad48963e6ae8c117f780e3b37dc5c7240df
tatevmel <tatevmelikyan90@gmail.com>
2020-05-12 18:36:39 +0400

    Task 208: Print true or false according if digits in the n number are equal.

--- src/com/jfk/thirdHomework/Task_208.java
+++ src/com/jfk/thirdHomework/Task_208.java
@@ -2,9 +2,9 @@ package com.jfk.thirdHomework;
 
 public class Task_208 {
     public static void main(String[] args) {
-        int n = 4444;
+        int n = 1234;
         int a = 0;
-        boolean t = true;
+        boolean t = false;
 
         while (n != 0) {
             a = n % 10;
@@ -13,8 +13,6 @@ public class Task_208 {
             if (n == a) {
                 t = true;
                 break;
-            } else {
-                t = false;
             }
         }
         System.out.println(t);
