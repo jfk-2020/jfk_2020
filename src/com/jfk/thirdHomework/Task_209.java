808a9ff5045f4b29044ba8d8bf14b155120dbdc2
tatevmel <tatevmelikyan90@gmail.com>
2020-05-12 18:35:21 +0400

    Task 209: Print true or false according if there is odd number in the given n.

---
+++ src/com/jfk/thirdHomework/Task_209.java
@@ -0,0 +1,20 @@
+package com.jfk.thirdHomework;
+
+public class Task_209 {
+    public static void main(String[] args) {
+        int n = 8857;
+        int a = 0;
+        boolean t = false;
+
+        while (n != 0) {
+            a = n % 10;
+            n = n / 10;
+
+            if (a % 2 != 0) {
+                t = true;
+                break;
+            }
+        }
+        System.out.println(t);
+    }
+}
