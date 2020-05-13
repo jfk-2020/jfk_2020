9efd7dba983aa9c08a1627366d3ff6d92e21e6db
ArmenHanisyan <hanisyan94@gmail.com>
2020-05-13 07:22:02 -0700

    Task151

---
+++ src/com/jfk/Task_151.java
@@ -0,0 +1,15 @@
+package com.jfk;
+
+public class Task_151 {
+    public static void main(String[] args) {
+        int n = 53;
+        int sum = 0;
+        for (int i = n; i < 10_000_0; i++) {
+            if (i % n == 0) {
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
