6488b8b28502b70217ef52d2f0bc0aa64ffc475d
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-13 12:24:35 +0400

    Task204

---
+++ src/Homework_201_210/Task_204.java
@@ -0,0 +1,16 @@
+package Homework_201_210;
+
+public class Task_204 {
+    public static void main(String[] args) {
+        int n = 2154;
+        int mianish= n%10;
+        int erknish=n%100 / 10;
+        int eranish= n%1000 /100;
+        int qaranish= n/1000;
+
+        System.out.println(mianish);
+        System.out.println(erknish);
+        System.out.println(eranish);
+        System.out.println(qaranish);
+    }
+}
