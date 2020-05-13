afcc02ab4be10b2cc9372328afcf898ffaaf2d66
tatevmel <tatevmelikyan90@gmail.com>
2020-05-13 16:18:46 +0400

    Task 78

---
+++ src/com/jfk/homework03/Task_78.java
@@ -0,0 +1,16 @@
+package com.jfk.homework03;
+
+public class Task_78 {
+    public static void main(String[] args) {
+        for(int x = 10; x <= 20; x += 2){
+            double y;
+            if (x > 12) {
+                y = 3 * Math.log(x) / Math.log(3);
+            } else {
+                y = x*x*x;
+            }
+            System.out.println("x = " + x + " y = " + y);
+            }
+        }
+    }
+
