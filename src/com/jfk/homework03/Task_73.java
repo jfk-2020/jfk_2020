400601908feb372ef1e9531a0362d55e2416a1be
tatevmel <tatevmelikyan90@gmail.com>
2020-05-13 15:42:55 +0400

    Task 73

---
+++ src/com/jfk/homework03/Task_73.java
@@ -0,0 +1,10 @@
+package com.jfk.homework03;
+
+public class Task_73 {
+    public static void main(String[] args) {
+        for (double x = 7.5; x <= 12.5; x += 0.2) {
+            double y = (x + 1) * (x + 1);
+            System.out.println("x = " + x + " y =" + y);
+        }
+    }
+}
