e5385bae38f2173dd3de37b68e503343dc7caf87
Vahan <Vahanaleq1988@gmail.com>
2020-05-14 15:53:35 +0400

    Task_33

---
+++ src/com/jfk/Task_33.java
@@ -0,0 +1,15 @@
+package com.jfk;
+
+public class Task_33 {
+    public static void main(String[] args) {
+        int a = 66;
+        int b = 47;
+        int c = 7;
+        int d = 3;
+
+        boolean y = (a == 1 || b == 1 || c ==1 || d ==1);
+        System.out.println(y);
+    }
+    }
+
+
