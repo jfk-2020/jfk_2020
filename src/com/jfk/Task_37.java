afbfcb0f45b352df0a36c313add1cef7eeac7b50
Vahan <Vahanaleq1988@gmail.com>
2020-05-14 20:05:51 +0400

    Task_37

---
+++ src/com/jfk/Task_37.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_37 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+        int c = 26;
+        int d = 58;
+        boolean x = (b-a==8 && c-b==16 && d-c==32);
+        System.out.println(x);
+    }
+}
