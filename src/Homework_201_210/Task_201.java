29698d1536299b2ece40e4b1b0f3d933a208f886
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-15 10:18:37 +0400

    Task201

--- src/Homework_201_210/Task_201.java
+++ src/Homework_201_210/Task_201.java
@@ -4,10 +4,10 @@ public class Task_201 {
     public static void main(String[] args) {
         int n = 2222222;
         int tvanshanneriQanak =0;
-       for (int i= 1 ; i <= n ; i = i * 10 ){
-           tvanshanneriQanak= tvanshanneriQanak+1;
+        for (int i= 1 ; i <= n ; i = i * 10 ){
+            tvanshanneriQanak= tvanshanneriQanak+1;
 
-}
-     System.out.println(tvanshanneriQanak);
-}
+        }
+        System.out.println(tvanshanneriQanak);
+    }
 }
