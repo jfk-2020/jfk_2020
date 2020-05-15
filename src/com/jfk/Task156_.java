390b3be82f4e63c5164ecef48d7243a79302c449
Vahan <Vahanaleq1988@gmail.com>
2020-05-15 17:33:37 +0400

    Task_156_

---
+++ src/com/jfk/Task156_.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class Task156_ {
+    public static void main(String[] args) {
+        int n = 3;
+        int sum = 0 ;
+
+        for ( int i = 10 ;i>= 10 && i < 100; i+=3 ) {
+
+            sum = sum + i; }
+        System.out.println(sum);
+    }
+}
