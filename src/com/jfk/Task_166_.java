6b75dc987a33c20af4c0150ffd8706b05f3b6e04
Vahan <Vahanaleq1988@gmail.com>
2020-05-15 20:41:02 +0400

    Task_166_

---
+++ src/com/jfk/Task_166_.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_166_ {
+    public static void main(String[] args) {
+        int n = 6561;
+        int y = 0;
+        for (int i = 0 ; i < 10000000 ; i++){
+            if ( (i * i * i * i == n)){y=1;}
+        }
+        System.out.println(y);
+    }
+}
