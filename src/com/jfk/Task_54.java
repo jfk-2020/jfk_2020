2c55edbf324eaacd4fa68926a3239fc9633a6aa2
Vahan <Vahanaleq1988@gmail.com>
2020-05-15 11:58:06 +0400

    Task_54

---
+++ src/com/jfk/Task_54.java
@@ -0,0 +1,18 @@
+package com.jfk;
+
+public class Task_54 {
+    public static void main(String[] args) {
+        int number = 768;
+        int f = (number % 10);
+        int m = (number % 100) / 10;
+        int l = (number % 1000) / 100;
+
+        if (f > m && m > l) {
+            System.out.println(f);
+        } else if (m > f && f > l){
+            System.out.println(m);
+        } else {
+            System.out.println(l);
+        }
+    }
+}
