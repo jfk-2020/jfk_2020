fbbd6eb893b9b25305959ad051d6925aa95ad87a
Vahan <Vahanaleq1988@gmail.com>
2020-05-15 12:41:19 +0400

    Task_59

---
+++ src/com/jfk/Task_59.java
@@ -0,0 +1,25 @@
+package com.jfk;
+
+public class Task_59 {
+    public static void main(String[] args) {
+        int number = 654;
+        int f = (number % 10);
+        int m = (number % 100) / 10;
+        int l = (number % 1000) / 100;
+
+        if (f < m && m < l) {
+            System.out.println(f+" "+m+" "+l);
+        } else if (f < l && l < m) {
+            System.out.println(f+" "+l+" "+m);
+        } else if (m < f && f < l) {
+            System.out.println(m+" "+f+" "+l);
+        } else if (m < l && l < f) {
+            System.out.println(m+" "+l+" "+f);
+        } else if (l < f && f < m) {
+            System.out.println(l+" "+f+" "+m);
+        } else if (l < m && m < f) {
+            System.out.println(l+" "+m+" "+f);
+        }
+    }
+}
+
