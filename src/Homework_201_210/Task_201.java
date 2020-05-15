22c506e86e4b8b23ce8661f37bfdd75e3e0b3c09
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-15 10:17:00 +0400

    Task209

--- src/Homework_201_210/Task_201.java
+++ src/Homework_201_210/Task_201.java
@@ -2,16 +2,12 @@ package Homework_201_210;
 
 public class Task_201 {
     public static void main(String[] args) {
-        int n = 10064;
-        int tvanshanneriQanak = 0;
-        while ( n>=1){
-            if (n <10){tvanshanneriQanak = 1;break;}
-            else{ if (n>=10 && n < 100){tvanshanneriQanak = 2;break;}
-            else{if (n>=100 && n <1000){tvanshanneriQanak = 3;break;}else {if (n>=1000 && n <10000){tvanshanneriQanak = 4;break;}
-            else {if (n>=10000 && n <100000){tvanshanneriQanak = 5;break;}}}}
+        int n = 2222222;
+        int tvanshanneriQanak =0;
+       for (int i= 1 ; i <= n ; i = i * 10 ){
+           tvanshanneriQanak= tvanshanneriQanak+1;
 
-            }n = n+1;
-        }
-        System.out.println(tvanshanneriQanak);
-    }
+}
+     System.out.println(tvanshanneriQanak);
+}
 }
