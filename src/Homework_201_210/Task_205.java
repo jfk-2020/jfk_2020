c0d20d0dc94532634ee6c8d52b7ca546f1f68806
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-17 10:50:37 +0400

    Task205

--- src/Homework_201_210/Task_205.java
+++ src/Homework_201_210/Task_205.java
@@ -1,19 +1,22 @@
 package Homework_201_210;
 
-public class Task_205{
+public class Task_205 {
     public static void main(String[] args) {
 
-    int n = 2154;
-    int mianish= n%10;
-    int erknish=n%100 / 10;
-    int eranish= n%1000 /100;
-    int qaranish= n/1000;
-
-        System.out.println(qaranish);
-        System.out.println(eranish);
-        System.out.println(erknish);
-        System.out.println(mianish);
-
-
+        int n = 212554;
+        int l = 0;
+        for (int k = 1; k <= n; k *= 10) {
+            l = l + 1;
+        }
+        int t = 1;
+        for (int f = 0; f < l - 1; f++) {
+            t = t * 10;
+        }
+        int m = 1;
+        for (int i = t; i >= 1; i /= 10) {
+            m = n / i % 10;
+            System.out.println(m);
+        }
     }
 }
+
