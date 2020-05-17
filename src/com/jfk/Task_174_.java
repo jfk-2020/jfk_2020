ada85441629ff3ed79b9180baa8140e8abed1b2b
Vahan <Vahanaleq1988@gmail.com>
2020-05-17 20:23:15 +0400

    Task_174_

---
+++ src/com/jfk/Task_174_.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class Task_174_ {
+    public static void main(String[] args) {
+        int N = 10;
+        double x = 2;
+        System.out.println(x);
+        for (int k = 2; k <= N; k++){
+            x = 2 + 1/x;
+            System.out.println(x);
+        }
+    }
+}
