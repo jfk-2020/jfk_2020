209bd80ec3c619b7f36b6484ad9e14d936b366e4
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-19 17:02:50 +0400

    Task210

--- src/Homework_201_210/Task_210.java
+++ src/Homework_201_210/Task_210.java
@@ -2,17 +2,14 @@ package Homework_201_210;
 
 public class Task_210 {
     public static void main(String[] args) {
-        int n = 5396;
-        int mianish= n%10;
-        int erknish=n%100 / 10;
-        int eranish= n%1000 /100;
-        int qaranish= n/1000;
+        int n = 2635;
+
          int z = 0;
          int k = 0;
-         if (mianish % 2 == 0){z= z + mianish;} else {k = k + mianish;}
-        if (erknish % 2 == 0){z= z + erknish;} else {k = k + erknish;}
-        if (eranish % 2 == 0){z= z + eranish;} else {k = k + eranish;}
-        if (qaranish % 2 == 0){z= z + qaranish;} else {k = k + qaranish;}
+         for (int i = 1 ; i <= n ; i*=10){
+             int t = n/i %10;
+             if (t % 2 ==0){z = z +t;} else {k = k + t;}
+         }
 
         boolean h = z==k;
         System.out.println(h);
