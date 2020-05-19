92db553c5564a32d2c9948cb5ba46a497e704487
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-19 10:05:27 +0400

    Task206

--- src/Homework_201_210/Task_206.java
+++ src/Homework_201_210/Task_206.java
@@ -2,13 +2,19 @@ package Homework_201_210;
 
 public class Task_206 {
     public static void main(String[] args) {
-        int n = 2154;
-        int mianish= n%10;
-        int erknish=n%100 / 10;
-        int eranish= n%1000 /100;
-        int qaranish= n/1000;
 
-        int m = mianish * 1000 + erknish * 100 + eranish * 10 + qaranish;
-        System.out.println(m);
+        int n = 25577;
+        double tiv = 0;
+        double tvanshanneriQanak = 0;
+        for (int j = 1; j <= n; j = j * 10) {
+            tvanshanneriQanak = tvanshanneriQanak + 1;
+        }
+        for (int i = n; i != 0; i /= 10) {
+            tiv = tiv + i % 10 * Math.pow(10, tvanshanneriQanak - 1);
+            tvanshanneriQanak = tvanshanneriQanak - 1;
+        }
+        System.out.println(tiv);
+
     }
 }
+
