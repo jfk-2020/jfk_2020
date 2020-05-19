6c26db2f4f72eaed2a47e8508306eec64b6f4ceb
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-19 10:13:04 +0400

    Task207

--- src/Homework_201_210/Task_207.java
+++ src/Homework_201_210/Task_207.java
@@ -2,13 +2,14 @@ package Homework_201_210;
 
 public class Task_207 {
     public static void main(String[] args) {
-        int n = 3154;
-        int mianish= n%10;
-        int erknish=n%100 / 10;
-        int eranish= n%1000 /100;
-        int qaranish= n/1000;
+        int n = 3354824;
         boolean x = false;
-        if (mianish == 2 || erknish ==2 ||eranish == 2 || qaranish ==2){ x = true;}
+        int t = 0;
+         for ( int i = 1 ; i <= n ; i*= 10 ){
+             t = n / i % 10;
+
+             if (t == 2){x =true ;}
+         }
 
         System.out.println(x);
 
