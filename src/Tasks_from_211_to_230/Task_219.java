9d6f4de1ffd0c97b4fa576bc4e2b0362e6b2777e
spetrosyan <sergopetrosyan@gmail.com>
2020-05-19 20:25:43 +0400

    Tasks_219

--- src/Tasks_from_211_to_230/Task_219.java
+++ src/Tasks_from_211_to_230/Task_219.java
@@ -2,21 +2,22 @@ package Tasks_from_211_to_230;
 
 public class Task_219 {
     public static void main(String[] args) {
-        int[] numbers = new int[6];
+        int[] numbers = new int[7];
         numbers[0] = 5;
         numbers[1] = -6;
         numbers[2] = 7;
         numbers[3] = 8;
-        numbers[4] = 9;
+        numbers[4] = 6;
         numbers[5] = -4;
+        numbers[6] = -4;
         int k = 2;
         int count = 0;
 
         for (int i = 0; i < numbers.length ; i++) {
             if (i % k == 0){
                 count++;
-                System.out.println(numbers[i]);
             }
         }
+        System.out.println(count);
     }
 }
