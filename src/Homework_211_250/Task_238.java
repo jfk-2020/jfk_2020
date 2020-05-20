522450a14c65820a1db49a8aa6a97eaf83ef2485
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-20 17:34:55 +0400

    Task238

--- src/Homework_211_250/Task_238.java
+++ src/Homework_211_250/Task_238.java
@@ -4,16 +4,18 @@ import com.jfk.ArrayElementGenerator;
 
 public class Task_238 {
     public static void main(String[] args) {
-        int [] ints =  ArrayElementGenerator.generateAsRandom(12);
+        int[] ints = ArrayElementGenerator.generateAsRandom(12);
         ArrayElementGenerator.displayArray(ints);
-        int sum = 0;
+        double sum = 0;
         int count = 0;
 
-        for (int i = 3 ; i < ints.length ; i+=3 ){
-             sum = sum + ints[i] ;
-             count = count + 1;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] % 3 == 0) {
+                sum = sum + ints[i];
+                count = count + 1;
+            }
         }
-        System.out.println((double)sum /count);
+        System.out.println((double) sum / count);
 
     }
 }
