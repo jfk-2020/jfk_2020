402c4e6699ac7adee102a7b2f5c1571ba58a208b
William Arustamyan <williamarustamyan@workfront.com>
2020-05-20 19:48:13 +0400

    task 289 review

--- src/com/jfk/homework05/homework281_290/Task_289.java
+++ src/com/jfk/homework05/homework281_290/Task_289.java
@@ -2,14 +2,15 @@ package com.jfk.homework05.homework281_290;
 
 public class Task_289 {
     public static void main(String[] args) {
-        int[] x = new int[]{1, 2, 4, 7, 9, 16, 49, 83, 4};
-        int count = 0;
         int c = 1;
         int d = 7;
+        int[] x = new int[]{1, 2, 4, 7, 9, 16, 49, 83, 4};
 
+
+        int count = 0;
         for (int i = 0; i < x.length; i++) {
             double square = Math.pow(x[i], 2);
-            if (square >= c && square <= d) {
+            if (square > c && square < d) {
                 count++;
             }
         }
@@ -18,9 +19,8 @@ public class Task_289 {
 
         for (int i = 0; i < x.length; i++) {
             double square = Math.pow(x[i], 2);
-            if (square >= c && square <= d) {
-                y[z] = x[i];
-                z++;
+            if (square > c && square < d) {
+                y[z++] = x[i];
             }
         }
 
