9429c9d82b62cfd255c6a96dc502e8e5fd0e13f2
tatevmel <tatevmelikyan90@gmail.com>
2020-05-22 12:30:55 +0400

    Tasks 281-290

--- src/com/jfk/homework05/homework281_290/Task_289.java
+++ src/com/jfk/homework05/homework281_290/Task_289.java
@@ -2,10 +2,13 @@ package com.jfk.homework05.homework281_290;
 
 public class Task_289 {
     public static void main(String[] args) {
-        int[] x = new int[]{1, 2, 4, 7, 9, 16, 49, 83, 4};
-        int count = 0;
+
         int c = 1;
         int d = 7;
+
+        int[] x = new int[]{1, 2, 4, 7, 9, 16, 49, 83, 4};
+        int count = 0;
+
 
         for (int i = 0; i < x.length; i++) {
             double square = Math.pow(x[i], 2);
