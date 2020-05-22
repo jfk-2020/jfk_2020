8328f7fc101466e4046544d3d28ce6841e083ee7
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-22 17:34:30 +0400

    Task281

--- src/Homework_281_290/Task_281.java
+++ src/Homework_281_290/Task_281.java
@@ -6,21 +6,23 @@ public class Task_281 {
     public static void main(String[] args) {
 //        int []ints = ArrayElementGenerator.generateAsRandom(6);
 //        ArrayElementGenerator.displayArray(ints);
-        int []ints ={2 ,5 , -5 , 0 , 4 , 6 , -8 , 7};
-        int [] y ;
-        int counter  = 0;
+        int[] ints = {2, 5, -5, 0, 4, 6, -8, 7};
+        int[] y;
+        int counter = 0;
 
-        for (int i = 0; i < ints.length;i++){
-            if (ints[i]> 0){counter ++; }
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] > 0) {
+                counter++;
+            }
 
         }
-        if (counter > 0){
-            y = new int[ counter ] ;
+        if (counter > 0) {
+            y = new int[counter];
             int index = 0;
-            for (int i = 0 ; i < ints.length ; i++){
-               if (ints[i] >0){
-                   y[index++]= ints[i];
-               }
+            for (int i = 0; i < ints.length; i++) {
+                if (ints[i] > 0) {
+                    y[index++] = ints[i];
+                }
             }
             ArrayElementGenerator.displayArray(y);
         }
