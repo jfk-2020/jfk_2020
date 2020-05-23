057efe776dfe2235a1622ab4e6d26c98b232ed1a
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-23 12:56:41 +0400

    Task286

---
+++ src/Homework_281_290/Task_286.java
@@ -0,0 +1,27 @@
+package Homework_281_290;
+
+import com.jfk.ArrayElementGenerator;
+
+public class Task_286 {
+    public static void main(String[] args) {
+            int[] ints = {2, 5, -5, 0, 4, 6, -8, 7};
+            int y[];
+            int counter = 0;
+            for (int i = 0; i < ints.length; i++) {
+                if (ints[i] % 2 == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter > 0) {
+                y = new int[counter];
+                int index = 0;
+                for (int i = 0; i < ints.length; i++) {
+                    if (ints[i] % 2 == 0) {
+                        y[index++] = ints[i];
+                    }
+                }
+                ArrayElementGenerator.displayArray(y);
+            }
+        }
+
+}
