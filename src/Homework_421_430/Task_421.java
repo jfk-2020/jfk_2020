9578a131023d8d9ab8e6b0063e76bb7ec1d53ee4
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-23 15:37:22 +0400

    Task421

---
+++ src/Homework_421_430/Task_421.java
@@ -0,0 +1,25 @@
+package Homework_421_430;
+
+import com.jfk.ArrayElementGenerator;
+
+public class Task_421 {
+    public static void main(String[] args) {
+
+        int[][] ints = {
+                {3, 8, -4, 6},
+                {2,-9,  6, 7},
+                {1, 0, -4, 5},
+                {3, 4, -8, 1},
+        };
+        int l = 3;
+        int count  = 0;
+        for (int i = 0;i < ints.length ;i++)
+
+        {for (int k =0; k< i+1 ; k++){if (ints[i][k] % l == 0){count ++;};
+        System.out.print(ints[i][ k]+" " );}
+            System.out.println();
+
+    }
+        System.out.println(count);
+}
+}
