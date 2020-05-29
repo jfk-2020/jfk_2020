872c59d08b3220e3ac7391a9ba9b032bde5e62f0
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-29 22:34:30 +0400

    Task657

--- src/Homework_646_660/Task_657.java
+++ src/Homework_646_660/Task_657.java
@@ -2,7 +2,7 @@ package Homework_646_660;
 
 public class Task_657 {
     public static void main(String[] args) {
-        int n = 7;
+        int n = 8;
         char[] char1 = {'l', 'x', 'j', 'k', 'x', 'a','k','o'};
         char[] char2 = new char[char1.length ];
         int x = 0;
