17da39ba7437def610c075d357b44b67a7a4bccf
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-05-30 09:03:23 +0400

    Task689

--- src/Homework_686_694/Task_689.java
+++ src/Homework_686_694/Task_689.java
@@ -3,7 +3,6 @@ package Homework_686_694;
 public class Task_689 {
     public static void main(String[] args) {
         char[] char1 = {'a', '6', 'x', '5', '1', 'P', 'c', 'Z'};
-        int count = 0;
         for (int i = 0; i< char1.length;i++){
             if (char1[i] >=97 && char1[i]<=122){char1[i]-=32;}
         }
