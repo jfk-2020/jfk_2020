e316bb658f9379f8c0800699998876437f2a3964
tatevmel <tatevmelikyan90@gmail.com>
2020-06-02 00:41:29 +0400

    A small change in task 659

--- src/com/jfk/homework08/Task_659.java
+++ src/com/jfk/homework08/Task_659.java
@@ -2,29 +2,22 @@ package com.jfk.homework08;
 
 public class Task_659 {
     public static void main(String[] args) {
-        char[] chars = {'c', 'd', 'c', 'd', 'd', 'd', 'c'};
+        char[] chars = {'c', 'x', 'c', 'd', 'd', 'd', 'c'};
 
         boolean x = false;
+        int countC = 0;
+        int countD = 0;
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == 'x') {
                 x = true;
+            } else if(chars[i] == 'c'){
+                countC++;
+            }else if(chars[i] == 'd'){
+                countD++;
             }
         }
-        int countC = 0;
-        int countD = 0;
 
-        for (int i = 0; i < chars.length; i++) {
-            if (x == true) {
-                if (chars[i] == 'c') {
-                    countC++;
-                }
-            } else {
-                if (chars[i] == 'd') {
-                    countD++;
-                }
-            }
-        }
-        if (x == true) {
+        if (x) {
             System.out.println(countC);
         } else {
             System.out.println(countD);
