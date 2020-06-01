fd6aa101f1390e48d82e25baeb7b4a0001e4caec
Artur Chilingaryan <arturchil@gmail.com>
2020-06-01 22:18:33 +0400

    Tasks 505 - 508

---
+++ src/com/jfk/Lesson07/Tasks505_515/Task507.java
@@ -0,0 +1,34 @@
+package com.jfk.Lesson07.Tasks505_515;
+
+import com.jfk.Lesson06.ArrayGenerator;
+import com.jfk.Lesson07.MatrixGenerator;
+
+public class Task507 {
+    public static void main(String[] args) {
+        int k = 5;
+        int[][] z = MatrixGenerator.generateAsRandom(k, k);
+        MatrixGenerator.display(z);
+        System.out.println("------------------------------------");
+        int[] b = new int[k];
+        for (int i = 0; i < z.length; i++){
+            b[i] = 0;
+            boolean isNegative = false;
+            for (int j = 0; j < z[i].length; j++) {
+                if (z[i][j] < 0 && !isNegative) {
+                    isNegative = true;
+                    continue;
+                }
+                if (z[i][j] < 0) {
+                    b[i] = 100;
+                    break;
+                } else {
+                    b[i] += z[i][j];
+                }
+            }
+            if (z[i][z[i].length - 1] < 0) {
+                b[i] = 100;
+            }
+        }
+        ArrayGenerator.displayArray(b);
+    }
+}
