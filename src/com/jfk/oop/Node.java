c8f9f470d569b76090abe7581a7fdc08c124f9d2
William Arustamyan <williamarustamyan@workfront.com>
2020-06-15 22:34:26 +0400

    inheritance

--- src/com/jfk/oop/Node.java
+++ src/com/jfk/oop/Node.java
@@ -8,4 +8,22 @@ public class Node {
 
     int value;
     Node child;
+
+    public Node(int value, Node child){
+        this.value = value;
+        this.child = child;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Node(10, new Node(12, null)));
+    }
+
+    @Override
+    public String toString() {
+        if (child == null) {
+            return "Node : { value : " + value + ", child : null }";
+        }
+        return "Node : { value : " + value + ", child : " + child.toString() + " }";
+    }
 }
