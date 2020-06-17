a1a0fe745973d2388a8328cb01aa84f904f0431d
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-06-17 15:46:34 +0400

    Electrical Engine

---
+++ src/Constructors/ElectricalEngine.java
@@ -0,0 +1,14 @@
+package Constructors;
+
+public class ElectricalEngine {
+    private String start;
+
+    public ElectricalEngine(String start){
+        this.start = start;
+    };
+
+    public void startEngine (){
+        System.out.println("Engine is started");
+    }
+
+}
