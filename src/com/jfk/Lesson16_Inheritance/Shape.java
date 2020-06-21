3889b6e046303753077b0da3cefc4725de71d743
Artur Chilingaryan <arturchil@gmail.com>
2020-06-21 18:43:08 +0400

    Lesson 14 Inheritance

---
+++ src/com/jfk/Lesson16_Inheritance/Shape.java
@@ -0,0 +1,37 @@
+package com.jfk.Lesson16_Inheritance;
+
+public class Shape {
+    private String color = "red";
+    private boolean filled = true;
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public Shape() {}
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    @Override
+    public String toString() {
+        return String.format("Shape[color=%s, filled=%b]", color, filled);
+    }
+}
