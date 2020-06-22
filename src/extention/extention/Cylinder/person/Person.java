d38a89f78790b8b17d1ae06a5452491f79f3e050
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-06-22 17:56:37 +0400

    Staff

---
+++ src/extention/extention/Cylinder/person/Person.java
@@ -0,0 +1,34 @@
+package extention.extention.Cylinder.person;
+
+import java.security.PublicKey;
+
+public class Person {
+    protected String name;
+    protected String address;
+
+    public Person(){};
+
+    public Person(String name , String address) {
+        this.name = name;
+        this.address = address;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
