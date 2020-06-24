3b624019e9f44a34a528471c95f792d4cc5c922d
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-06-24 10:44:06 +0400

    Student

--- src/extention/extention/Cylinder/person/Student.java
+++ src/extention/extention/Cylinder/person/Student.java
@@ -10,7 +10,6 @@ private double fee;
 
 public Student (String name , String address , String program , int year , double fee){
 super(name, address);
-
 this.program = program;
 this.year = year;
 this.fee = fee;
