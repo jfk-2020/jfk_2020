4b172c000ab39ad93553d088213466631842db1f
Artur Chilingaryan <arturchil@gmail.com>
2020-06-26 19:10:00 +0400

    Lesson 17 BookStore2

--- src/com/jfk/Lesson17_BookStore/BookStore.java
+++ src/com/jfk/Lesson17_BookStore/BookStore.java
@@ -1,4 +1,15 @@
 package com.jfk.Lesson17_BookStore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookStore {
+    private List<Book> store = new ArrayList<Book>();
+
+    public void add(Book book) {
+        store.add(book);
+    }
+    public void remove(Book book) {
+        store.remove(book);
+    }
 }
