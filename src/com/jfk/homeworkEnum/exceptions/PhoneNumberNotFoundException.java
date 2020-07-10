350bb70166d9728ab89d3100b5230d3521de3c55
William Arustamyan <williamarustamyan@workfront.com>
2020-07-10 19:40:42 +0400

    code review

--- src/com/jfk/homeworkEnum/exceptions/PhoneNumberNotFoundException.java
+++ src/com/jfk/homeworkEnum/exceptions/PhoneNumberNotFoundException.java
@@ -1,6 +1,6 @@
 package com.jfk.homeworkEnum.exceptions;
 
-public class PhoneNumberNotFoundException extends Exception {
+public class PhoneNumberNotFoundException extends IllegalArgumentException {
     public PhoneNumberNotFoundException(String message) {
         super(message);
     }
