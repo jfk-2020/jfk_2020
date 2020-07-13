630088c26e0abd98f88ce67d8288ee1fffb567a9
William Arustamyan <williamarustamyan@workfront.com>
2020-07-13 19:26:32 +0400

    review

--- src/Save_files/BufferedInputStream.java
+++ src/Save_files/BufferedInputStream.java
@@ -1,29 +1,31 @@
 package Save_files;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class BufferedInputStream {
 
-    public BufferedInputStream(FileInputStream fileInputStream, int i) {
-
-    }
 
     public static void main(String[] args) throws IOException {
 
         FileInputStream fileInputStream = new FileInputStream("C:\\Users\\spetrosyan\\Desktop\\javaprogram.txt");
 
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 200);
+        java.io.BufferedInputStream bis = new java.io.BufferedInputStream(fileInputStream, 200);
 
         int i;
 
-        while((i = bufferedInputStream.read())!= -1){
+        while ((i = bis.read()) != -1) {
 
-            System.out.print((char)i);
+            System.out.print((char) i);
         }
-    }
 
-    private char read() {
-        return 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\spetrosyan\\Desktop\\javaprogram.txt"))) {
+            reader.readLine();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
