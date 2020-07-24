62f6df7a4dc60fe30a6ad9f9d0c70bb7df7c902d
Artur Chilingaryan <arturchil@gmail.com>
2020-07-24 09:47:07 +0400

    ArrayListOwn not ended

--- src/com/jfk/lesson23Generics/ArrayListOwn.java
+++ src/com/jfk/lesson23Generics/ArrayListOwn.java
@@ -1,20 +1,170 @@
 package com.jfk.lesson23Generics;
 
-public class ArrayListOwn <T> {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ArrayListOwn <T> implements List<T> {
     private int size = 0;
-    private Object[] array = new Object[1000];
-    public void add(Object a) {
-        if (size >= 1000) {
-            throw new ArrayIndexOutOfBoundsException();
+    private Object[] array = new Object[10];
+
+    @Override
+    public boolean add(T t) {
+        if (size >= array.length) {
+            Object[] arrayTemp = new Object[array.length + (int)(array.length * 0.25)];
+            for (int i = 0; i < size; i++) {
+                arrayTemp[i] = array[i];
+            }
+            array = arrayTemp;
         }
-        array[size++] = a;
+        array[size++] = t;
+        return true;
     }
+
+    @Override
     public T get(int index) {
         if (index >= size) {
             throw new NullPointerException();
         } else {
             return (T) array[size - 1];
         }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Object a: array) {
+            if(a.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] arraytemp = new Object[size];
+        for (int i = 0; i < size; i++) {
+            arraytemp[i] = array[i];
+        }
+        return arraytemp;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (contains(o)) {
+            int sizeTemp = 0;
+            Object[] arrayTemp = new Object[array.length];
+            for (int i = 0; i < array.length; i++){
+                if (array[i].equals(o)) {
+                    continue;
+                }
+                arrayTemp[sizeTemp++] = array[i];
+            }
+            size = sizeTemp;
+            array = arrayTemp;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object a: c){
+            if (!contains(a)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        for (T a : c) {
+            add(a);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        array = new Object[10];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
     }
 
     public static void main(String[] args) {
