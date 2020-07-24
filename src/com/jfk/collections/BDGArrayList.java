9484b646cd6b3a1840fbd3d7b71f4b678a031a05
tatevmel <tatevmelikyan90@gmail.com>
2020-07-24 18:40:08 +0400

    Collection implementation task

--- src/com/jfk/collections/BDGArrayList.java
+++ src/com/jfk/collections/BDGArrayList.java
@@ -3,25 +3,36 @@ package com.jfk.collections;
 import java.util.Collection;
 import java.util.Iterator;
 
-/**
- * @author William Arustamyan
- */
-
-
 public class BDGArrayList<E> implements Collection<E> {
+
+    private E data[];
+    private int size = 0;
+
+    public BDGArrayList(int size) {
+        this.data = (E[]) new Object[size];
+    }
+
+    public BDGArrayList() {
+        this(10);
+    }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(o)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -45,11 +56,32 @@ public class BDGArrayList<E> implements Collection<E> {
 
     @Override
     public boolean add(E e) {
-        return false;
+        if (size >= data.length) {
+            E[] finalArray = (E[]) new Object[data.length + 30];
+            System.arraycopy(data, 0, finalArray, 0, data.length);
+            finalArray[++size] = e;
+            data = finalArray;
+        } else {
+            data[++size] = e;
+        }
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(o)) {
+                E[] newArray = (E[]) new Object[data.length + -1];
+                for (int j = 0, k = 0; j < data.length; j++) {
+                    if (i == index) {
+                        continue;
+                    }
+                    newArray[k++] = data[j];
+                }
+                return true;
+            }
+        }
         return false;
     }
 
