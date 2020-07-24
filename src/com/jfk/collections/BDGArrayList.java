a5e3c97d638a952d8f7cf9a682c826e83bd15463
sergeypetrosyan <sergopetrosyan@gmail.com>
2020-07-24 16:30:49 +0400

    BDG

--- src/com/jfk/collections/BDGArrayList.java
+++ src/com/jfk/collections/BDGArrayList.java
@@ -10,18 +10,41 @@ import java.util.Iterator;
 
 public class BDGArrayList<E> implements Collection<E> {
 
+    private int minCapacity = 10;
+    private int currentIndex;
+    private Object[] elements;
+
+    public BDGArrayList() {
+        elements = new Object[minCapacity];
+        currentIndex = 0;
+    }
+
+    public BDGArrayList(int minCapacity) {
+        if (minCapacity > 0)
+            this.minCapacity = minCapacity;
+        elements = new Object[minCapacity];
+        currentIndex = 0;
+    }
+
     @Override
     public int size() {
-        return 0;
+
+        return currentIndex;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+
+        return currentIndex == 0;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < elements.length; i++) {
+            Object ob = elements[i];
+            if (o.equals(ob))
+                return true;
+        }
         return false;
     }
 
@@ -45,36 +68,110 @@ public class BDGArrayList<E> implements Collection<E> {
 
     @Override
     public boolean add(E e) {
+        if (e != null) {
+            if (elements.length == currentIndex) {
+                Object[] objects = new Object[elements.length];
+                for (int i = 0; i < elements.length; i++) {
+                    objects[i] = elements[i];
+                }
+                elements = new Object[(int) (minCapacity * 1.5)];
+                for (int i = 0; i < elements.length; i++) {
+                    elements[i] = objects[i];
+                }
+            }
+            elements[currentIndex++] = e;
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean remove(Object o) {
+        if (o != null) {
+            if (contains(o)) {
+                for (int i = 0; i < elements.length; i++) {
+                    if (elements[i].equals(o)) {
+                        remove(i);
+                        return true;
+                    }
+                }
+            }
+
+        }
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        if (c.size() > this.size()) {
+            return false;
+        }
+        Object[] objects = c.toArray();
+        for (int i = 0; i < objects.length; i++) {
+            Object ob = objects[i];
+            if (ob != null && !contains(ob))
+                return false;
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
+        if (c != null) {
+            if (elements.length <= currentIndex + c.size()) {
+                Object[] objects = new Object[elements.length];
+                for (int i = 0; i < elements.length; i++) {
+                    objects[i] = elements[i];
+                }
+                elements = new Object[currentIndex + c.size()];
+                for (int i = 0; i < elements.length; i++) {
+                    elements[i] = objects[i];
+                }
+            }
+            Object[] objects = c.toArray();
+            for (int i = 0; i < objects.length; i++) {
+                Object ob = objects[i];
+                this.add((E) ob);
+                return true;
+            }
+        }
         return false;
     }
 
+
     @Override
     public boolean removeAll(Collection<?> c) {
+        Object[] objects = c.toArray();
+        if (c.containsAll(this)) {
+            for (int i = 0; i < objects.length; i++) {
+                Object ob = objects[i];
+                this.remove(ob);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        int i = 0;
+        boolean t = false;
+        while (i < currentIndex) {
+            if (!c.contains(elements[i])) {
+                this.remove(i);
+                t = true;
+                continue;
+            }
+            i++;
+        }
+        return t;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        currentIndex = 0;
     }
 }
